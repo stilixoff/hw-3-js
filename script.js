4367aad5531119ca7f1d844c06a64f51fcbdfb1f
Oleh Starodubets <stilixoff@gmail.com>
2020-02-09 22:01:41 +0200

    add 7 function

--- script.js
+++ script.js
@@ -49,7 +49,34 @@ function countLetter(word = "Асталависта", letter = "а") {
 
 console.log(countLetter());
 
-function convertCurrency(cash) {
+function convertCurrency(cash = '100$') {
     
+    let correctCurrency;
+
+    if (cash.indexOf("$") !== -1) {
+        correctCurrency = ((parseInt(cash)) * 25) + "UAH"; 
+    } else if (cash.indexOf("uah" || "UAH") !== -1) {
+        correctCurrency = ((parseInt(cash)) / 25) + "$";
+    } else {
+        correctCurrency = "я не могу посчитать эту валюту";
+    }
+    
+    return correctCurrency;
 }
 
+console.log(convertCurrency());
+
+
+
+
+
+
+
+document.writeln (`Создать функцию getMaxDigit(number) – которая получает любое число и выводит самую большую цифру в этом числе (123456789) = ${getMaxDigit()} <br>
+Создать функцию, которая вычисляет степень числа. Не используя Math.pow и **. Используйте цикл. 5 в 3й степени = ${powFunction(5, 3)} <br>
+Создать функцию, которая форматирует имя, делая первую букву заглавной. oleH = ${uppercase()} <br>
+Создать функцию, которая вычисляет сумму оставшуюся после оплаты налога от заработной платы. 1000 и 19,5% = ${(tax(1000, 19.5))} <br>
+Создать функцию, которая возвращает случайное целое число в диапазоне от N до M от 10 до 100 = ${randomNumber()} <br>
+Создать функцию, которая считает сколько раз определенная буква повторяется в слове Асталависта буква "а" = ${countLetter()} <br>
+Создайте функцию конвертиующую доллары в гривны и наоборот в зависимости от наличия символа $ или UAH в строке = ${convertCurrency()}
+`);
